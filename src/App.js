c0967e680ba270724588dfe991d62d9159dbbfbb
Henry <dev.mcdreamy@gmail.com>
2025-03-07 10:58:39 +0200

    add: effect environment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three'
 import { useLayoutEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, Float, Preload } from '@react-three/drei'
+import { useGLTF, Float, Preload, OrbitControls } from '@react-three/drei'
 import { Effects } from './Effects'
 
 export default function App() {
   return (
     <Canvas gl={{ antialias: false, stencil: false }} camera={{ position: [5, 0, 0], fov: 80 }}>
-      <ambientLight intensity={6} />
+      <ambientLight intensity={0.6} />
       <spotLight
         angle={0.12}
         penumbra={0.1}
@@ -18,6 +18,39 @@ export default function App() {
           self.target.updateMatrixWorld()
         }}
       />
+      <Hall position={[0, 0.98, 0]} />
+      <Float>
+        <Probe position={[-1.75, 0.25, -0.85]} scale={0.025} rotation={[0, Math.PI / 2, 0]} />
+      </Float>
+      <Effects />
+      <Rig from={-Math.PI / 2} to={Math.PI / 2.66} />
+      <Preload all />
     </Canvas>
   )
+}
+
+function Rig() {
+  useFrame((state) => {
+    state.camera.position.lerp({ x: 0, y: -state.pointer.y / 4, z: state.pointer.x / 2 }, 0.1)
+    state.camera.lookAt(-1, 0, 0)
+  })
+}
+
+function Hall({ ...props }) {
+  const { scene } = useGLTF('/hall-transformed.glb')
+  return <primitive object={scene} {...props} />
+}
+
+function Probe({ ...props }) {
+  const { scene, materials } = useGLTF('/probe-transformed.glb')
+  useLayoutEffect(() => {
+    Object.values(materials).forEach((material) => (material.roughness = 0))
+    Object.assign(materials.light, {
+      color: new THREE.Color('#0066FF'),
+      emissive: new THREE.Color(0, 1, 0),
+      emissiveIntensity: 2,
+      toneMapped: false
+    })
+  }, [])
+  return <primitive object={scene} {...props} />
 }
